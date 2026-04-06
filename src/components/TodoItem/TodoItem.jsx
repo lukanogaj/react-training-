e18d20d4cebf1164dbc17aff26175331671567d1
Lukasz Nogaj <lukasz.nogajdev@gmail.com>
2026-04-06 11:26:52 +0100

    refactor(todo-item): separate title and status badge, fix styling and UI consistency

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -2,18 +2,30 @@ import styles from "./TodoItem.module.scss";
 
 const TodoItem = ({ todo, onComplete, onDelete }) => {
 	return (
-		<div>
-			{todo.title} {todo.completed && <span>Done</span>}
-			<button
-				className={styles.btn}
-				onClick={() => onComplete(todo.id)}>
-				Complete
-			</button>
-			<button
-				className={styles.btn}
-				onClick={() => onDelete(todo.id)}>
-				Delete
-			</button>
+		<div className={styles.container}>
+			<div className={styles.titleRow}>
+				<span
+					className={`${styles.title} ${todo.completed ? styles.titleCompleted : ""}`}>
+					{todo.title}
+				</span>
+				<span className={`${todo.completed ? styles.status : ""} `}>
+					{todo.completed && "Done"}
+				</span>
+			</div>
+			<div className={styles.actions}>
+				{!todo.completed && (
+					<button
+						className={styles.completedButton}
+						onClick={() => onComplete(todo.id)}>
+						Complete
+					</button>
+				)}
+				<button
+					className={styles.deleteButton}
+					onClick={() => onDelete(todo.id)}>
+					Delete
+				</button>
+			</div>
 		</div>
 	);
 };
